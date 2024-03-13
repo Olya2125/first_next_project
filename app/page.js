e3dfc60d094bdf0d17b74cf397e6bc16c93359b4
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-03-13 15:03:33 +0300

    added links

--- app/page.js
+++ app/page.js
@@ -8,7 +8,8 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 Welcome to Next.js course
         </p>
-<Link href='/blog'>Click here</Link>
+        <p >
+          <a href="/blog">Click here</a></p>
       </div>
  
     </main>
